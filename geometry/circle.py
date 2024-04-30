8a602993ce57978f96cafeecb038a75e42690423
harshitlohani19 <harshitlohani2002@gmail.com>
2024-04-30 21:36:46 +0530

    Lint Fix

--- geometry/circle.py
+++ geometry/circle.py
@@ -1,13 +1,7 @@
-"""
-importing math module
-"""
 import math
 
 
 class Circle:
-    """
-    Circle class definition
-    """
     def __init__(self, radius):
         """
         Constructor to initialize the radius.
@@ -16,35 +10,35 @@ class Circle:
 
     def area(self):
         """
-        Return the area of the circle.
+        Returns the area of the circle.
         """
         area = math.pi * self.radius**2
         return area
 
     def circumference(self):
         """
-        Method to calculate and return the circumference of the circle.
+        Calculates and return the circumference of the circle.
         """
         circumference = 2 * math.pi * self.radius
         return circumference
 
     def __str__(self):
         """
-        Method to return a string representation of the Circle object.
+        Returns a string representation of the Circle object.
         """
         str_repr = f"Circle radius: {self.radius}"
         return str_repr
 
     def __eq__(self, other):
         """
-        Method to check if the areas of two circles are equal.
+        Checks if the areas of two circles are equal.
         """
         aeq = self.area() == other.area()
         return aeq
 
     def __lt__(self, other):
         """
-        Method to check if the area of the current
+        Checks if the area of the current
         circle is less than the area of another circle.
         """
         lt = self.area() < other.area()
