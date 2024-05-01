f994c568f883132ad80ff85d09a3141dd89b23a1
harshitlohani19 <harshitlohani2002@gmail.com>
2024-05-01 16:06:08 +0530

    fix

--- main.py
+++ main.py
@@ -11,16 +11,8 @@ def main():
         radius = input("Enter Radius: ")
         try:
             radius = float(radius)
-<<<<<<< HEAD
-            if radius > 0:
                 circle = Circle(float(radius))
                 print(circle)
-            else:
-                print("Radius cannot be negative")
-=======
-            circle = Circle(float(radius))
-            print(circle)
->>>>>>> a4fe171 (')
         except ValueError:
             print("Invalid Radius")
     elif figure == "square":
