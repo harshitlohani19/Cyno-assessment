8342f612074a5f3b6cbe4086612f6a8f9ecbce64
nivesh <nivesh.nk1000@gmail.com>
2024-05-02 14:51:25 +0530

    fixed doc string

--- geometry/square.py
+++ geometry/square.py
@@ -5,26 +5,35 @@ class Square:
     @property
     def area(self) -> float:
         """
-        Returns the area of the square.
+        Calculates the area of the square.
+        @params:
+        @return: float
+
         """
         return self.side_length**2
 
     @property
     def perimeter(self) -> float:
         """
-        Returns the perimeter of the square.
+        Calculates the perimeter of the square.
+        @params:
+        @return: float
         """
         return 4 * self.side_length
 
     def __str__(self) -> str:
         """
         Returns the string representation of the square.
+        @params:
+        @return: str
         """
         return f"Square with side length {self.side_length}"
 
     def __eq__(self, other) -> bool:
         """
         Returns True if their areas are equal, False otherwise.
+        @params:
+        @return: bool
         """
         return self.side_length == other.side_length
 
@@ -32,5 +41,7 @@ class Square:
         """
         Returns True if the area of the current square is less
         than the area of the other square, False otherwise.
+        @params:
+        @return: bool
         """
         return self.side_length < other.side_length
