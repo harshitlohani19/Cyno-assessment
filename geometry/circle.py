dec1067355e6e996e45f7d6fe286a0f216b814ec
harshitlohani19 <harshitlohani2002@gmail.com>
2024-05-02 14:51:32 +0530

    Doc string Fix

--- geometry/circle.py
+++ geometry/circle.py
@@ -3,15 +3,14 @@ import math
 
 class Circle:
     def __init__(self, radius) -> None:
-        """
-        Constructor to initialize the radius.
-        """
         self.radius = radius
 
     @property
     def area(self) -> float:
         """
-        Returns the area of the circle.
+        Calculates the area of the circle.
+        @params:
+        @return: float
         """
         area = math.pi * self.radius**2
         return area
@@ -19,7 +18,9 @@ class Circle:
     @property
     def circumference(self) -> float:
         """
-        Returns the circumference of the circle.
+        Calculates the circumference of the circle.
+        @params:
+        @return: float
         """
         circumference = 2 * math.pi * self.radius
         return circumference
@@ -27,6 +28,8 @@ class Circle:
     def __str__(self) -> str:
         """
         Returns a string representation of the Circle object.
+        @params:
+        @returns: str
         """
         str_repr = f"Circle radius: {self.radius}"
         return str_repr
@@ -34,6 +37,8 @@ class Circle:
     def __eq__(self, other) -> bool:
         """
         Checks if the areas of two circles are equal.
+        @params:
+        @returns:bool
         """
         aeq = self.radius == other.radius
         return aeq
@@ -42,6 +47,8 @@ class Circle:
         """
         Checks if the area of the current
         circle is less than the area of another circle.
+        @params:
+        @returns:bool
         """
         lt = self.radius < other.radius
         return lt
